dfc8c6cc695f62b82b9f544b4c2dbb7ecabd1000
MatthewCAlbert <matthewmcavaio@gmail.com>
2021-01-26 21:58:29 +0700

    add readme + fix seed

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tugas Database Matkul System
+
+## seed.sql
+- Contain initial dummy data to start demo of this project
--- seed.sql
+++ seed.sql
@@ -23,33 +23,13 @@ INSERT INTO Rooms VALUES
     (14,6,304,NULL),
     (15,6,305,NULL);
 
-INSERT INTO Users VALUES
-    (1,'klein.rico@gmail.com','wsNFH6wd','M','Rico','Klein','4626 Washington Avenue','United States','(870) 572-9510','1998-02-12 00:00:00',NULL),
-    (2,'mcgrath_awesome@gmail.com','xqNbFcUb','F','Della','McGrath','576 Retreat Avenue','United States','(916) 485-1279','30/11/1998',NULL),
-    (3,'clay_funny_khloe@yahoo.co.id','gaFu9mTf','M','Khloe','Clay','673 Leo Street','United States','(972) 347-9049','13/09/1998',NULL),
-    (4,'jerome_cobbzs1@outlook.com','ujbEn5LN','M','Jerome','Cobb','217 Jane St West Helena, Arkansas(AR), 72390','United States','(301) 290-1443','1978-10-10 00:00:00',NULL),
-    (5,'fritzbby_Niko@gmail.com','TdKHgAsC','M','Niko','Fritz','1802 Grey Ave Evanston, Illinois(IL), 60201','United States','(765) 932-2732','2001-01-01 00:00:00',NULL),
-    (6,'wallaby102@yahoo.com','Mqkln2j2l','M','Paul','Wallaby','1 Wilmott St','United Kingdom','(456) 667-8923','1990-02-12 00:00:00',NULL),
-    (7,'Marsten_loop@yahoo.com','dfLmaA9Q','M','John','Marten','3 St Margaret St','United Kingdom','(901) 889-2850','13/03/1988',NULL),
-    (8,'master.yoda@yahoo.com','ADgdhen0','M','Yondu','Yoda','Westminster','United Kingdom','(902) 764-9022','2001-11-08 00:00:00',NULL),
-    (9,'cika_alba@yahoo.com','1danILM','F','Jessica','Alba','2-14 Myddleton St','United Kingdom','(782) 110-2920','19/05/2002',NULL),
-    (10,'aquaspace7@yahoo.com','7H7JJkml','F','Vannesa','Bigs','Aglionby St','United Kingdom','(782) 098-2222','20/06/2002',NULL);
 
-INSERT INTO Payments VALUES
-    (1,150000,'Credit Card','Success','2021-01-05 09:31:59'),
-    (2,1000000,'Debit Card','Waiting','2021-01-11 20:27:12'),
-    (3,250000,'Credit Card','Failed','2021-01-10 14:18:56'),
-    (4,350000,'Debit Card','Success','2021-01-06 07:18:12'),
-    (5,265000,'Debit Card','Success','2021-01-01 23:09:49');
+EXEC createAccount @email='klein.rico@gmail.com', @password='wsNFH6wd', @title='Mr', @first_name='Rico', @last_name='Klein', @address='4626  Washington Avenue', @country='United States', @phone_num='(870) 572-9510', @birth_date='1996-12-01';
+
+EXEC createAccount @email='klein.ddoogami@gmail.com', @password='wsNFH6wddas', @title='Mr', @first_name='Rico', @last_name='Bucher', @address='320 KfC', @country='Indonesia', @phone_num='+62 8111', @birth_date='1993-12-01';
+
 
 INSERT INTO Coupons VALUES
     (NEWID(),'APP15','2021-01-04 00:00:00','2021-01-29 00:00:00',3,20000),
     (NEWID(),'CHEAP01','2021-01-05 00:00:00','2021-01-06 00:00:00',1,100000),
     (NEWID(),'BOOK14','2021-01-15 12:00:00','2021-01-16 00:00:00',1,50000);
-
-INSERT INTO Bookings VALUES
-    (101,4,11,1,NULL,'2021-01-26 10:08:27','2021-01-29 19:15:55','Success','2021-01-26 12:00:31'),
-    (102,5,12,2,222,'2021-01-25 02:40:14','2021-01-25 13:40:14','Success','2021-01-24 13:40:14'),
-    (103,7,13,3,NULL,'2021-01-17 01:33:25','2021-01-30 12:51:25','Failed','2021-01-16 20:33:25'),
-    (104,9,14,4,NULL,'2021-01-16 02:19:09','2021-01-21 14:48:54','Request Cancel','2021-01-15 21:19:09'),
-    (105,10,15,5,111,'2021-01-20 07:10:31','2021-01-24 08:02:56','Success','2021-01-20 02:02:56');
